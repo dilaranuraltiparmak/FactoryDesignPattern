3c96af245fe5f08dffd0f9481f93d8cfc59c3ffa
Dilara Nur Altıparmak <78371701+dilaranuraltiparmak@users.noreply.github.com>
2023-03-14 09:41:37 +0300

    Add project files.

---
+++ FactoryDesignPattern.UI/FormKart.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FactoryDesignPattern.UI
+{
+    partial class FormKart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKart));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPlatinum = new System.Windows.Forms.Button();
+            this.btnMoney = new System.Windows.Forms.Button();
+            this.btnTitanium = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 130);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(291, 159);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 130);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(547, 159);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(196, 117);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(318, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kartlar";
+            // 
+            // btnPlatinum
+            // 
+            this.btnPlatinum.Location = new System.Drawing.Point(335, 329);
+            this.btnPlatinum.Name = "btnPlatinum";
+            this.btnPlatinum.Size = new System.Drawing.Size(123, 23);
+            this.btnPlatinum.TabIndex = 4;
+            this.btnPlatinum.Text = "Özellik Göster";
+            this.btnPlatinum.UseVisualStyleBackColor = true;
+            this.btnPlatinum.Click += new System.EventHandler(this.btnPlatinum_Click);
+            // 
+            // btnMoney
+            // 
+            this.btnMoney.Location = new System.Drawing.Point(86, 329);
+            this.btnMoney.Name = "btnMoney";
+            this.btnMoney.Size = new System.Drawing.Size(105, 23);
+            this.btnMoney.TabIndex = 5;
+            this.btnMoney.Text = "Özellik Göster";
+            this.btnMoney.UseVisualStyleBackColor = true;
+            this.btnMoney.Click += new System.EventHandler(this.btnMoney_Click);
+            // 
+            // btnTitanium
+            // 
+            this.btnTitanium.Location = new System.Drawing.Point(608, 329);
+            this.btnTitanium.Name = "btnTitanium";
+            this.btnTitanium.Size = new System.Drawing.Size(83, 23);
+            this.btnTitanium.TabIndex = 6;
+            this.btnTitanium.Text = "Özellik Göster";
+            this.btnTitanium.UseVisualStyleBackColor = true;
+            this.btnTitanium.Click += new System.EventHandler(this.btnTitanium_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTitanium);
+            this.Controls.Add(this.btnMoney);
+            this.Controls.Add(this.btnPlatinum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPlatinum;
+        private System.Windows.Forms.Button btnMoney;
+        private System.Windows.Forms.Button btnTitanium;
+    }
+}
+
---
+++ FactoryDesignPattern.UI/FormKart.cs
@@ -0,0 +1,73 @@
+﻿using FactoryDesignPattern.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactoryDesignPattern.UI
+{
+    public partial class FormKart : Form
+    {
+        public FormKart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlatinum_Click(object sender, EventArgs e)
+        {
+            ICreditCard cardDetails = (ICreditCard)CreditCardFactory.GetCreditCard("Platinum");
+      
+            if (cardDetails != null)
+            {
+                MessageBox.Show("CardType : " + cardDetails.GetCardType() +" "+
+                    "AnnualCharge :" + cardDetails.GetAnnualCharge());
+             
+            }
+            else
+            {
+                MessageBox.Show("Invalid Card Type");
+            }
+         
+        }
+
+        private void btnMoney_Click(object sender, EventArgs e)
+        {
+            ICreditCard cardDetails = (ICreditCard)CreditCardFactory.GetCreditCard("MoneyBack");
+
+            if (cardDetails != null)
+            {
+                MessageBox.Show("CardType : " + cardDetails.GetCardType() + " " +
+                    "AnnualCharge :" + cardDetails.GetAnnualCharge());
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Card Type");
+            }
+        }
+
+        private void btnTitanium_Click(object sender, EventArgs e)
+        {
+            ICreditCard cardDetails = (ICreditCard)CreditCardFactory.GetCreditCard("Titanium");
+
+            if (cardDetails != null)
+            {
+                MessageBox.Show("CardType : " + cardDetails.GetCardType() + " " +
+                    "AnnualCharge :" + cardDetails.GetAnnualCharge());
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Card Type");
+            }
+        }
+
+    }
+    }
+    
+
